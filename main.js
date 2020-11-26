fe98e0238adf4d18edb5babe6a9cd90f401fee27
iamsarthakk <sarthakkumar160627@gmail.com>
2020-11-26 12:39:02 +0530

    Responive Dashboard

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
       $('.sidebar .collapse').collapse('hide');
+      $('.sidebar').addClass('d-none');
+      $('.card-container').removeClass('col-9')
+      $('.card-container').addClass('col-12')
+    }
+    else{
+      $('.sidebar').removeClass('d-none');
+      $('.card-container').removeClass('col-12')
+      $('.card-container').addClass('col-9')
     };
   });
 
@@ -12,20 +20,26 @@ $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
   $(window).resize(function() {
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
+      // $("body").addClass("sidebar-toggled");
+      // $(".sidebar").addClass("toggled");
+      // $('.sidebar').addClass('d-none');
     };
     
     // Toggle the side navigation when window is resized below 480px
     if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
       $("body").addClass("sidebar-toggled");
       $(".sidebar").addClass("toggled");
+      $(".sidebar").addClass("d-none");
+      $('.sidebar .collapse').collapse('hide');
+    };
+    if ($(window).width() > 768) {
+      // $("body").addClass("sidebar-toggled");
+      // $(".sidebar").addClass("toggled");
+      $(".sidebar").removeClass("d-none");
       $('.sidebar .collapse').collapse('hide');
     };
 
-    if ($(window).width() > 480) {
-        $("body").addClass("sidebar-toggled");
-        $(".sidebar").removeClass("toggled");
-        $('.sidebar .collapse').collapse('hide');
-      };
+    
   });
 
   
